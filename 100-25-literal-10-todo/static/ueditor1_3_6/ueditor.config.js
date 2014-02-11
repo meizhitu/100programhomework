57c5295d9207011f703cd7eadebcafe442609428
meizhitu <meizhitu@gmail.com>
2014-02-11 23:13:08 +0800

    web.py with ueditor

--- 100-25-literal-10-todo/static/ueditor1_3_6/ueditor.config.js
+++ 100-25-literal-10-todo/static/ueditor1_3_6/ueditor.config.js
@@ -30,8 +30,8 @@
         UEDITOR_HOME_URL : URL
 
         //图片上传配置区
-        ,imageUrl:URL+"php/imageUp.php"             //图片上传提交地址
-        ,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
+        ,imageUrl:"/ue_imageUp"             //图片上传提交地址
+        ,imagePath:""                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         //,imageFieldName:"upfile"                  //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
         //,compressSide:0                           //等比压缩的基准，确定maxImageSideLength参数的参照对象。0为按照最长边，1为按照宽度，2为按照高度
         //,maxImageSideLength:900                   //上传图片最大允许的边长，超过会自动等比缩放,不缩放就设置一个比较大的值，更多设置在image.html中
@@ -44,8 +44,8 @@
         ,scrawlPath:URL+"php/"                            //图片修正地址，同imagePath
 
         //附件上传配置区
-        ,fileUrl:URL+"php/fileUp.php"               //附件上传提交地址
-        ,filePath:URL + "php/"                   //附件修正地址，同imagePath
+        ,fileUrl:"/ue_fileUp"               //附件上传提交地址
+        ,filePath:""                   //附件修正地址，同imagePath
         //,fileFieldName:"upfile"                    //附件提交的表单名，若此处修改，需要在后台对应文件修改对应参数
 
         //远程抓取配置区
