a41b80817904876fa7063947ce64912e6b55cc26
meizhitu <meizhitu@gmail.com>
2014-02-14 11:04:12 +0800

    add beautifulsoup and news ticker,relate with re,urllib2

--- 100-26-literal-11.py
+++ 100-26-literal-11.py
@@ -7,6 +7,9 @@ pygtk.require("2.0")
 import gtk
 import urllib2
 from BeautifulSoup import BeautifulSoup
+import re
+import urllib, math
+from datetime import datetime
 
 
 class NewsTickerWindow:
@@ -28,12 +31,30 @@ class NewsTickerWindow:
 
     def delayrun(self):
         self.count += 1
-        self.lbgame.set_text(str(self.count))
+        #self.lbgame.set_text(str(self.count))
         content = urllib2.urlopen("http://roll.news.sina.com.cn/").read()
         soup = BeautifulSoup(content)
         self.lbnews.set_label(soup.li.span.a.string)
         self.lbnews.set_uri(soup.li.span.a['href'])
-        print(soup.li.span.a)
+        #content = urllib2.urlopen("http://bdcjhq.hexun.com/quote?s2=000001.sh").read()
+        #soup = BeautifulSoup(content)
+        #print(content)
+        #m = re.search('pc:"([^"]+)"', content)
+        #self.lbgame.set_text(m.groups()[0])
+        #print(m.groups()[0])
+        t = datetime.now()
+        h = t.hour
+        m = t.minute
+        s = math.floor(t.second / 10) * 10
+        arg = "%02d%02d%02d" % (h, m, s)
+        params = urllib.urlencode({"stocklist": "000001_1|399001_2", "time": arg})
+        f = urllib2.urlopen("http://quote.tool.hexun.com/hqzx/getstock.aspx?%s" % params)
+        content = f.read()
+        m = re.search("(\['[^\]]+\])", content)
+        print(m.groups()[0])
+        arr = eval(m.groups()[0])
+        print(arr[3])
+        self.lbgame.set_text("上证指数：" + str(arr[3]))
         return True
 
 
